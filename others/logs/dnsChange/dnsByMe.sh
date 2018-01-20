4108f6251640f091929dfb54ca89cd4d5a6e9a92
root <root@localhost.localdomain>
2018-01-20 17:15:31 -0500

    adjust dnschange something

--- others/logs/dnsChange/dnsByMe.sh
+++ others/logs/dnsChange/dnsByMe.sh
@@ -3,5 +3,7 @@ cat /dev/null > /etc/resolv.conf
 while read ns
 do
 	echo $ns >> /etc/resolv.conf
-done < /home/hudson/myGithub/linuxStudying/others/logs/dnsChange/dnsByMe.conf
-/etc/init.d/networking restart
+done < /root/myGithub/linuxStudying/others/logs/dnsChange/dnsByMe.conf
+# /etc/init.d/networking restart
+systemctl restart systemd-networkd
+systemctl restart systemd-resolved
