2844b253693974043dd35cba72a3b9a820b1c9f1
myHost <wuhaishui1234@gmail.com>
2018-12-07 21:28:59 +0800

    rm some old files

--- bash/my/dnsChange/dnsByMe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cat /dev/null > /etc/resolv.conf
-while read ns
-do
-	echo $ns >> /etc/resolv.conf
-done < /root/myGithub/linuxStudying/others/logs/dnsChange/dnsByMe.conf
-# /etc/init.d/networking restart
-systemctl restart systemd-networkd
-systemctl restart systemd-resolved
